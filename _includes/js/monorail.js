6ba65eac5a70edd687b3b39be189626299a519e9
Zacky Ma <marchbox@hey.com>
2022-03-13 22:13:56 -0700

    avoid private properties for safari

--- _includes/js/monorail.js
+++ _includes/js/monorail.js
@@ -7,22 +7,22 @@ export class Monorail extends HTMLElement {
   };
   static willNavigateAway = true;
 
-  #shouldAnimate = true;
-  #intersectObserver;
+  shouldAnimate = true;
+  intersectObserver;
 
   connectedCallback() {
-    this.#intersectObserver = new IntersectionObserver(entries => {
-      this.#shouldAnimate = entries[0].isIntersecting;
+    this.intersectObserver = new IntersectionObserver(entries => {
+      this.shouldAnimate = entries[0].isIntersecting;
     }, {
       // Only use page transition animation when at least half of the monorail
       // element is intersecting with the viewport.
       threshold: .5,
     });
-    this.#intersectObserver.observe(this);
+    this.intersectObserver.observe(this);
 
     document.documentElement.addEventListener('turbo:before-visit', evt => {
       if (this.constructor.willNavigateAway &&
-          this.#shouldAnimate &&
+          this.shouldAnimate &&
           evt.detail.url !== window.location.href) {
         evt.preventDefault();
         document.body.classList.add(this.constructor.ClassName.PAGE_OUT);
@@ -40,6 +40,6 @@ export class Monorail extends HTMLElement {
   }
 
   disconnectCallback() {
-    this.#intersectObserver?.disconnect();
+    this.intersectObserver?.disconnect();
   }
 }
