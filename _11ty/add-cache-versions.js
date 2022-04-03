ac89eb1716aee0ebc25594120d0515d358375272
Zacky Ma <zacky@marchbox.com>
2022-04-02 20:27:43 -0700

    fix build

--- _11ty/add-cache-versions.js
+++ _11ty/add-cache-versions.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
-const cssVersion = fs.readFileSync(
-  path.join(__dirname, `../_tmp/cssVersion`)
-).toString();
-const jsVersion = fs.readFileSync(
-  path.join(__dirname, `../_tmp/jsVersion`)
-).toString();
-
 module.exports = function(content, outputPath) {
+  const cssVersion = fs.readFileSync(
+    path.join(__dirname, `../_tmp/cssVersion`)
+  ).toString();
+  const jsVersion = fs.readFileSync(
+    path.join(__dirname, `../_tmp/jsVersion`)
+  ).toString();
+
   if (outputPath && outputPath.endsWith('.html')) {
     content = content
         .replace('[css-version]', cssVersion)
