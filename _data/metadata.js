c751e1acf2fbd032d2db91e8b8c5860bd23db9df
Zacky Ma <zacky@marchbox.com>
2022-04-10 16:47:28 -0700

    change global data files to yaml

--- _data/metadata.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  title: 'MarchBox',
-  description: 'Dumplings, pixels, & the Mouse',
-  host: 'https://marchbox.com',
-  author: {
-    name: 'Zacky Ma',
-    email: 'zacky@marchbox.com',
-  },
-};
