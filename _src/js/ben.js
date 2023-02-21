45b1324ef304366827dfc016a851a3b21d7c020c
Zacky Ma <zacky@marchbox.com>
2023-02-21 01:23:26 +0000

    add ben (bio-electronic navigator) to use navigation api

---
+++ _src/js/ben.js
@@ -0,0 +1,76 @@
+export default class Ben {
+  constructor() {
+    this.#init();
+  }
+
+  #init() {
+    navigation.addEventListener('navigate', evt => {
+      if (!evt.canIntercept || evt.hashChange ||
+          evt.downloadRequest !== null ||
+          evt.formData !== null) {
+        return;
+      }
+
+      if (evt.destination.sameDocument) {
+        evt.preventDefault();
+        return;
+      }
+
+      evt.intercept({
+        handler: async () => {
+          try {
+            const page = await this.#fetchPage(evt.destination.url);
+            this.#renderPage(page);
+          } catch (err) {
+            throw err;
+          }
+        },
+      });
+    });
+  }
+
+  async #fetchPage(url) {
+    const res = await fetch(url, {
+      method: 'get',
+    });
+
+    if (res.ok) {
+      const html = await res.text();
+      return new DOMParser().parseFromString(html, 'text/html');
+    }
+
+    return null;
+  }
+
+  #renderPage(doc) {
+    if (!doc) {
+      return;
+    }
+
+    document.title = doc.title;
+
+    const {head, body} = doc;
+
+    head.querySelectorAll('meta').forEach(el => {
+      const name = el.name;
+
+      if (['viewport'].includes(name) || !name) {
+        return;
+      }
+
+      const current = document.querySelector(`meta[name="${name}"]`);
+
+      if (current) {
+        document.head.replaceChild(el, current);
+      } else {
+        document.head.append(el);
+      }
+    });
+
+    document.body.replaceChild(
+      body.querySelector('main'),
+      document.querySelector('main')
+    );
+  }
+}
+
--- _src/js/main.js
+++ _src/js/main.js
@@ -1,14 +1,22 @@
 import 'regenerator-runtime/runtime';
 import '@ungap/custom-elements-builtin';
 
+import Ben from './ben';
 import Glockenspiel from './glockenspiel';
 import Monorail from './monorail';
+import MonorailWithBen from './monorail-with-ben';
 import Sidewalks from './sidewalks';
 import Tulgeywood from './tulgeywood';
 import {
   whenDocumentComplete,
   whenDocumentReady,
 } from './utils';
+
+const supportNavigation = 'navigation' in window;
+
+if (supportNavigation) {
+  new Ben();
+}
 
 whenDocumentReady().then(() => {
   if ('customElements' in window) {
@@ -20,6 +28,7 @@ whenDocumentReady().then(() => {
 
 whenDocumentComplete().then(() => {
   if ('customElements' in window) {
-    customElements.define('land-monorail', Monorail, {extends: 'nav'});
+    const MonorailClass = supportNavigation ? MonorailWithBen : Monorail;
+    customElements.define('land-monorail', MonorailClass, {extends: 'nav'});
   }
 });
