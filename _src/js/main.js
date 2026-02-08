7f883c4a47a28e38a7a2540d6fd83260c992ad4e
Zacky Ma <zacky@marchbox.com>
2026-02-07 17:05:18 -0800

    remove monorail js

--- _src/js/main.js
+++ _src/js/main.js
@@ -2,19 +2,7 @@ import "regenerator-runtime/runtime";
 import "@ungap/custom-elements-builtin";
 
 import Glockenspiel from "./glockenspiel.js";
-import Monorail from "./monorail.js";
 import Tulgeywood from "./tulgeywood.js";
-import { whenDocumentComplete, whenDocumentReady } from "./utils.js";
 
-whenDocumentReady().then(() => {
-	if ("customElements" in window) {
-		customElements.define("land-glockenspiel", Glockenspiel);
-		customElements.define("land-tulgeywood", Tulgeywood);
-	}
-});
-
-whenDocumentComplete().then(() => {
-	if ("customElements" in window) {
-		customElements.define("land-monorail", Monorail, { extends: "nav" });
-	}
-});
+customElements.define("land-glockenspiel", Glockenspiel);
+customElements.define("land-tulgeywood", Tulgeywood);
