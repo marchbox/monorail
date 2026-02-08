e2f34fa05f469410c338f9fb61d6e59f2a638c69
Zacky Ma <zacky@marchbox.com>
2026-02-08 01:02:23 -0800

    adjust active car position on mobile

--- _src/js/main.js
+++ _src/js/main.js
@@ -1,8 +1,24 @@
-import "regenerator-runtime/runtime";
-import "@ungap/custom-elements-builtin";
-
 import Glockenspiel from "./glockenspiel.js";
 import Tulgeywood from "./tulgeywood.js";
 
 customElements.define("land-glockenspiel", Glockenspiel);
 customElements.define("land-tulgeywood", Tulgeywood);
+
+// Center the active monorail car after animation. This can be removed after
+// `scroll-start-target` is supported.
+window.addEventListener("pagereveal", async (evt) => {
+	const activeMonorailCar = document.querySelector(".monorail-car.active");
+
+	if (!activeMonorailCar || !evt.viewTransition) {
+		return;
+	}
+
+	try {
+		await evt.viewTransition.finished;
+	} finally {
+		activeMonorailCar.scrollIntoView({
+			block: "nearest",
+			inline: "center",
+		});
+	}
+});
